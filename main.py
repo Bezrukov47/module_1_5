a84625747cfe3d5554a9159bf0b880268ec3e08d
Bezrukov47 <bezr47@gmail.com>
2024-11-18 18:05:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+immutable_var = ('Hello',  1947, 'Falce')
+print (immutable_var)
+#immutable_var [1] = 2001
+mutable_list = [1, 2, 2, 3, 5, 6, 'Bill']
+print(mutable_list)
+print (mutable_list[-1])
+print (mutable_list[4])
+mutable_list[3]=[800]
+print (mutable_list[3])
